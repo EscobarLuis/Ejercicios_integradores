1841eb88dc98d67bac84097bd33ae16bc0c765c2
EscobarLuis <escobarluis77@gmail.com>
2023-08-21 00:24:28 -0300

    Agrego recursividad al ejercicio 5.

--- ejercicios/ejercicio5.py
+++ ejercicios/ejercicio5.py
@@ -4,9 +4,24 @@ def get_int():
             dato = int(input("Ingrese un numero: "))
             return dato
         except ValueError:
-            print ("ERROR: Tipo de dato incorrecto.")
+            print("ERROR: Tipo de dato incorrecto.")
+
+
+def get_int_recursiva():
+
+    try:
+        dato = int(input("Ingrese un numero: "))
+        return dato
+    except ValueError:
+        print("ERROR: Tipo de dato incorrecto.")
+        get_int_recursiva()
+
 
 def main():
-    print(f'Usted ingreso el numero: {get_int()}') 
+    
+    print(f'Usted ingreso el numero: {get_int()}')
+
+    print(f'Usted ingreso el numero: {get_int_recursiva()}')
+
 if __name__ == "__main__":
     main()
